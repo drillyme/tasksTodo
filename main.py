62007d52f68fd1bcfb6f2199995e71729448bba8
drillyme <lakhanthakurboss@gmail.com>
2023-04-30 12:20:52 +0530

    fastApi

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, Body, Depends
+import schemas
+import models
+from mangum import Mangum
+
+from database import Base, engine, SessionLocal
+from sqlalchemy.orm  import Session
+
+Base.metadata.create_all(engine)
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+app = FastAPI()
+handler = Mangum(app)
+
+@app.get("/")
+def getItems(session: Session = Depends(get_session)):
+    items = session.query(models.Item).all()
+    return items
+
+@app.get("/{id}")
+def getItem(id:int, session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
+
+@app.post("/")
+def addItem(item:schemas.Item, session = Depends(get_session)):
+    item = models.Item(task = item.task)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+    return item
+
+@app.put("/{id}")
+def updateItem(id:int, item:schemas.Item, session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    itemObject.task = item.task
+    session.commit()
+    return itemObject
+
+@app.delete("/{id}")
+def deleteItem(id:int, session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    session.delete(itemObject)
+    session.commit()
+    session.close()
+    return 'Item was deleted'
